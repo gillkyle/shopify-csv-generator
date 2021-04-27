77a91d1cd16ea61a3f482658e095e47f6a1f7fa7
Matt Kane <matt@gatsbyjs.com>
2021-04-27 09:39:34 +0100

    Use colours for variants, not sizes

--- faker_script.rb
+++ faker_script.rb
@@ -2,7 +2,6 @@ require 'faker'
 require 'csv'
 require_relative 'product'
 require_relative 'simple_product'
-
 if ARGV.empty?
 	puts "Usage: ruby faker_script.rb [n]"
 	puts "n: number of products to genetate"
@@ -15,9 +14,11 @@ else
 		product = Product.new({})
 		csv << product.headers
 		count.times do 
-			product = Product.new({})
+			sizes = [Faker::Commerce.unique.color,Faker::Commerce.unique.color]
+			defaultsize = Faker::Commerce.unique.color
+			Faker::Commerce.unique.clear
+			product = Product.new({option1_value: defaultsize, option1_name: 'Color'})
 			csv << product.attributes
-			sizes = ['Small','Medium','Large']
 			sizes.each do |size|
 				csv << product.simple_product_attributes(size)
 			end
